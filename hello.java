3d4679534eb99444c0fc842f77b9b1d3fad34f96
kaz0918 <kazkigenki0918@gmail.com>
2023-07-08 21:44:03 +0900

    mainからも修正してみる

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class hello{
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println(1 + 2 * 3);
-      　System.out.println("branch2からも修正");
-      　System.out.println("補足:衝突してconflictsになることもある");
+        System.out.println("branch2からも修正");
+        System.out.println("補足:衝突してconflictsになることもある");
     }
 }
